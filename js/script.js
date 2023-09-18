a0ba3541ba282a0e6ad5745f15e041e2e94ed8e3
Desiree Morimoto <dmorimoto89@gmail.com>
2023-09-17 22:00:15 -0700

    Added search feature

--- js/script.js
+++ js/script.js
@@ -1,90 +1,111 @@
 // ELEMENT SELECTORS
 const authorContainer = document.querySelector("#authorContainer");
 const paginationList = document.querySelector("#paginationList");
+const searchInput = document.querySelector("#authorSearch")
 
 // console.log(authors);
 const authorsPerPage = 3;
 
-/* This function will handle calculating how many buttons are
+/* ENTER YOUR CODE HERE */
+
+// 1. Create an event listener on the searchInput to listen for keyboard input.
+searchInput.addEventListener('keyup', () => {
+  // 2. Inside, create a variable storing an empty array for the soon-to-be filtered authors.
+  const filteredAuthors = [];
+  // 3. Create a variable to store the string the user has typed.
+  //    Hint: toLowerCase()
+  const searchString = searchInput.value.toLowerCase()
+  // 4-a. Start a loop to the length of `authors`.
+  // 4-b. Inside, create a variable to store the current `authors` name.
+  //      Hint: toLowerCase()
+  // 4-c. Create a conditional to check if the author's name includes the user's input.
+  //      Hint: includes()
+  // 4-d. If true, push the current author object into the new array created above.
+  for (let i = 0; i < authors.length; i++) {
+    const currentAuthor = authors[i].name.toLowerCase();
+    if (currentAuthor.includes(searchString)) {
+      filteredAuthors.push(authors[i]);
+    }
+  }
+  // 5-a. Create a conditional to check if the length of the new array is greater than zero.
+  // 5-b. If true,
+  //      - Call the handlePagination() function passing it this new array.
+  //      - Call the showPage() function passing it this new array and the number 1.
+  // 5-c. If false,
+  //      - Set the authorContainer's innerHTML to an <h3> saying no results were found.
+  //      - Set the paginationList's innerHTML to an empty string.
+  if (filteredAuthors.length > 0) {
+    handlePagination(filteredAuthors);
+    showPage(filteredAuthors, 1);
+  } else {
+    authorContainer.innerHTML = '<h3>No results were found</h3>';
+    paginationList.innerHTML = '';
+  }
+});
+
+/* DON'T CHANGE THE CODE BELOW */
+
+/* This function handles calculating how many buttons are
 needed and dynamically add them to the page */
 
 function handlePagination(array) {
-  // 1. Create a variable to store the number of buttons needed.
-  //    The math should be (the length of the array divided by the authorsPerPage) rounded up
-  //    Hint: Math.ceil()
-  const numButtons = Math.ceil(array.length / authorsPerPage);
-  // 2-a. Start a loop to the length of the number of buttons calculated above.
-  // 2-b. Inside, create a variable storing a template literal of the HTML markup of a button
-  //      (see example in index.html lines 34 - 36).
-  // 2-c. Then add this variable to the paginationList element
-  //      Hint: insertAdjacentHTML()
-  for (let i = 1; i <= numButtons; i++) {
-    let html = `<li><button>${i}</button></li>`;
-    paginationList.insertAdjacentHTML('beforeend', html);
+  const numberOfButtons = Math.ceil(array.length / authorsPerPage);
+  paginationList.innerHTML = "";
+
+  for (let i = 1; i <= numberOfButtons; i++) {
+    const html = `
+      <li>
+        <button>${i}</button>
+      </li>
+    `;
+    paginationList.insertAdjacentHTML("beforeend", html);
   }
-  // 3. Add the `active` class to the first button
-  //    Hint: querySelector()
-  const btnFirst = document.querySelector('ul li button');
-  btnFirst.className = 'active';
+  paginationList.querySelector("button").classList.add("active");
+  
+  /* This event listener handles calling our function
+  below to change the page & add the `active` class  */
+
+  paginationList.addEventListener("click", (e) => {
+    const activeButton = paginationList.querySelector(".active");
+    const buttonClicked = e.target.closest("button");
+  
+    if (activeButton && buttonClicked) {
+      activeButton.classList.remove("active");
+    }
+  
+    if (buttonClicked) {
+      buttonClicked.classList.add("active");
+      showPage(array, buttonClicked.innerHTML);
+    }
+  });
 }
 
-/* This function will handle calculating how many and which
+/* This function handles calculating how many and which
 authors to show on the current page and dynamically add them */
 
 function showPage(array, page) {
-  // 4. Create a variable to represent which author to start with on the page.
-  //    The math should be (the page multiplied by the authorsPerPage) minus the authorsPerPage
-  const startingAuthor = (page * authorsPerPage) - authorsPerPage;
-  // 5. Create a variable to represent which author to end with on the page.
-  //    The math should be (the page multiplied by the authorsPerPage) minus one
-  const endingAuthor = (page * authorsPerPage) - 1;
-  // 6. Reset the authorContainer's content to nothing to prevent previous cards staying on the page
-  authorContainer.innerHTML = '';
-  // 7-a. Start a loop to the length of the array's length
-  // 7-b. Inside, create a conditional checking if `i` is...
-  //      - greater than or equal to the start variable
-  //      - less than or equal to the end variable
-  // 7-c. If true, create a variable storing a template literal of the HTML markup of an author card.
-  //      (see example in index.html lines 17 - 28).
-  //      Hint: You'll need to dynamically add each author's information
-  // 7-d. Then add this variable to the authorContainer element
-  //      Hint: insertAdjacentHTML()
+  const start = page * authorsPerPage - authorsPerPage;
+  const end = page * authorsPerPage - 1;
+  authorContainer.innerHTML = "";
+
   for (let i = 0; i < array.length; i++) {
-    if (i >= startingAuthor && i <= endingAuthor) {
-      let html = `<div class="author-card">
-      <div class="card-header">
-        <img src="${array[i].image}" alt="photo of ${array[i].name}" />
-      </div>
-      <div class="card-content">
-        <h2 class="title">${array[i].name}</h2>
-        <p>
-          ${array[i].text}
-        </p>
-      </div>
-    </div> `;
-      authorContainer.insertAdjacentHTML('beforeend', html);
+    if (i >= start && i <= end) {
+      const html = `
+        <div class="author-card">
+          <div class="card-header">
+            <img src="${array[i].image}" alt="photo of ${array[i].name}" />
+          </div>
+          <div class="card-content">
+            <h2 class="title">${array[i].name}</h2>
+            <p>${array[i].text}</p>
+          </div>
+        </div>
+      `;
+
+      authorContainer.insertAdjacentHTML("beforeend", html);
     }
   }
 }
-
-/* This event listener will handle calling our function
-above to change the page & add the `active` class  */
-
-paginationList.addEventListener("click", (e) => {
-  // 8. Create a variable to store the button which currently has the `active` class
-  const btnActive = document.querySelector('.active');
-  // 9-a. Make sure the user has clicked a `button`
-  //      Hint: e.target
-  // 9-b. If true...
-  //      - Remove the `active` class from the currently active button
-  //      - Add the `active` class to the button just clicked
-  //      - Call showPage() passing it `authors` and the content of the button just clicked.
-  if (e.target.tagName === "BUTTON") {
-    btnActive.classList.remove("active");
-    e.target.className = 'active';
-    showPage(authors, e.target.textContent);
-  }
-});
 
 /* These function calls are needed to initialize the page */
 
